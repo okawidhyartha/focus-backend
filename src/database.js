fee26293607b481d70f57572267f6322bcc80eda
Selna <selna.rustam@yahoo.com>
2024-11-14 19:33:22 +0700

    typo psdatabase

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 const client = new Client({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
-    database: process.env.PSDATABASE,
+    database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
 });
